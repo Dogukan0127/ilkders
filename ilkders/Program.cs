5f9525d9854ae0ab2508f71f8c8fb0b3caaf8e29
Dogukan0127 <dogukangs2005@gmail.com>
2020-11-25 10:12:01 +0300

    Proje dosyası ekle.

---
+++ ilkders/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ilkders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doğukan");//WriteLine yazar ve bir satır boşluk bırakır.
+            Console.Write("Ateş");//yazar ama aynı satırdan devam eder.
+            Console.Write(" Nasılsınız?");
+        }
+    }
+}
